f09bc4ab90084f364f08ef2fd6f871977ca7a0a8
root <root@38afac95d0a9>
2016-10-19 06:04:13 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "Testjaxbimpl",
+  "homePage" : "Main",
+  "name" : "Testjaxbimpl",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
